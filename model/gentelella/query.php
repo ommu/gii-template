f91505d9c3825afb67470f055c97e44ced864793
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-04-19 02:14:03 +0700

    commit

--- model/gentelella/query.php
+++ model/gentelella/query.php
@@ -16,7 +16,7 @@
 
 $modelFullClassName = $modelClassName;
 if ($generator->ns !== $generator->queryNs) {
-    $modelFullClassName = '\\' . $generator->ns . '\\' . $modelFullClassName;
+	$modelFullClassName = '\\' . $generator->ns . '\\' . $modelFullClassName;
 }
 
 echo "<?php\n";
@@ -31,26 +31,26 @@ namespace <?= $generator->queryNs ?>;
  */
 class <?= $className ?> extends <?= '\\' . ltrim($generator->queryBaseClass, '\\') . "\n" ?>
 {
-    /*public function active()
-    {
-        return $this->andWhere('[[status]]=1');
-    }*/
+	/*public function active()
+	{
+		return $this->andWhere('[[status]]=1');
+	}*/
 
-    /**
-     * @inheritdoc
-     * @return <?= $modelFullClassName ?>[]|array
-     */
-    public function all($db = null)
-    {
-        return parent::all($db);
-    }
+	/**
+	 * @inheritdoc
+	 * @return <?= $modelFullClassName ?>[]|array
+	 */
+	public function all($db = null)
+	{
+		return parent::all($db);
+	}
 
-    /**
-     * @inheritdoc
-     * @return <?= $modelFullClassName ?>|array|null
-     */
-    public function one($db = null)
-    {
-        return parent::one($db);
-    }
+	/**
+	 * @inheritdoc
+	 * @return <?= $modelFullClassName ?>|array|null
+	 */
+	public function one($db = null)
+	{
+		return parent::one($db);
+	}
 }
