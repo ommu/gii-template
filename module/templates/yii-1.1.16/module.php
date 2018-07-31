a174d4863b521315514a8d49d526773bb2356fdb
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-07-31 18:31:24 +0700

    commit

--- module/templates/yii-1.1.16/module.php
+++ module/templates/yii-1.1.16/module.php
@@ -42,24 +42,16 @@ class <?php echo $this->moduleClass; ?> extends CWebModule
 		// this method is called before any module controller action is performed
 		// you may place customized code here
 		// list public controller in this module
-		$publicControllers = array();
 		$controllerMap = array();
 
-		$controllerPath = Yii::getPathOfAlias('application.modules.'.$this->_module.'.controllers');
-		foreach (new DirectoryIterator($controllerPath) as $fileInfo) {
-			if($fileInfo->isDot())
-				continue;
-			
-			if($fileInfo->isFile() && !in_array($fileInfo->getFilename(), array('index.php'))) {
-				$getFilename = $fileInfo->getFilename();
-				$publicControllers[] = $controller = strtolower(preg_replace('(Controller.php)', '', $getFilename));
-				$controllerMap[$controller] = array(
-					'class'=>'application.modules.'.$this->_module.'.controllers.'.preg_replace('(.php)', '', $getFilename),
-				);
-			}
-		}
+		$controllerPath = 'application.modules.'.$this->_module.'.controllers';
+		if(!empty($controllerMap))
+			$controllerMap = array_merge($controllerMap, $this->getController($controllerPath));
+		else
+			$controllerMap = $this->getController($controllerPath);
+
 		$this->controllerMap = $controllerMap;
-		$this->publicControllers = $publicControllers;
+		$this->publicControllers = array_keys($this->controllerMap);
 	}
 
 	public function beforeControllerAction($controller, $action) 
@@ -87,5 +79,37 @@ class <?php echo $this->moduleClass; ?> extends CWebModule
 			$this->_assetsUrl = Yii::app()->getAssetManager()->publish(Yii::getPathOfAlias('<?php echo $this->moduleID; ?>.assets'));
 		
 		return $this->_assetsUrl;
+	}
+
+	public function getController($path, $sub=null)
+	{
+		$controllerMap = array();
+		$controllerPath = Yii::getPathOfAlias($path);
+		$pathArray = explode('.', $path);
+		$lastPath = end($pathArray);
+
+		foreach (new DirectoryIterator($controllerPath) as $fileInfo) {
+			if($fileInfo->isDot() && $fileInfo->isDir())
+				continue;
+			
+			if($fileInfo->isFile() && !in_array($fileInfo->getFilename(), array('index.php','.DS_Store'))) {
+				$getFilename = $fileInfo->getFilename();
+				$controller = strtolower(preg_replace('(Controller.php)', '', $getFilename));
+				if($lastPath != 'controllers')
+					$controller = join('', array($lastPath, preg_replace('(Controller.php)', '', $getFilename)));
+				$controllerClass = preg_replace('(.php)', '', $getFilename);
+
+				$controllerMap[$controller] = array(
+					'class'=>join('.', array($path, $controllerClass)),
+				);
+
+			} else if($fileInfo->isDir()) {
+				$sub = $fileInfo->getFilename();
+				$subPath = join('.', array($path, $sub));
+				$controllerMap = array_merge($controllerMap, $this->getController($subPath, $sub));
+			}
+		}
+		
+		return $controllerMap;
 	}
 }
