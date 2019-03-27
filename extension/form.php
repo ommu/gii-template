226ffe43c743dc5100bb392d057e248288768dca
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-03-27 10:09:16 +0700

    commit

--- extension/form.php
+++ extension/form.php
@@ -11,38 +11,38 @@
 </div>
 
 <?php
-echo $form->field($generator, 'vendorName', ['template' => '{label}{beginWrapper}{input}{error}{endWrapper}{hint}', 'horizontalCssClasses' => ['wrapper'=>'col-md-9 col-sm-9 col-xs-12 col-12']])
+echo $form->field($generator, 'vendorName', ['template' => '{label}{beginWrapper}{input}{error}{endWrapper}{hint}', 'horizontalCssClasses' => ['wrapper'=>'col-sm-9 col-xs-12 col-12']])
 	->label($generator->getAttributeLabel('vendorName'));
 
-echo $form->field($generator, 'packageName', ['template' => '{label}{beginWrapper}{input}{error}{endWrapper}{hint}', 'horizontalCssClasses' => ['wrapper'=>'col-md-9 col-sm-9 col-xs-12 col-12']])
+echo $form->field($generator, 'packageName', ['template' => '{label}{beginWrapper}{input}{error}{endWrapper}{hint}', 'horizontalCssClasses' => ['wrapper'=>'col-sm-9 col-xs-12 col-12']])
 	->label($generator->getAttributeLabel('packageName'));
 
-echo $form->field($generator, 'namespace', ['template' => '{label}{beginWrapper}{input}{error}{endWrapper}{hint}', 'horizontalCssClasses' => ['wrapper'=>'col-md-9 col-sm-9 col-xs-12 col-12']])
+echo $form->field($generator, 'namespace', ['template' => '{label}{beginWrapper}{input}{error}{endWrapper}{hint}', 'horizontalCssClasses' => ['wrapper'=>'col-sm-9 col-xs-12 col-12']])
 	->label($generator->getAttributeLabel('namespace'));
 
-echo $form->field($generator, 'type', ['template' => '{label}{beginWrapper}{input}{error}{endWrapper}{hint}', 'horizontalCssClasses' => ['wrapper'=>'col-md-9 col-sm-9 col-xs-12 col-12']])
+echo $form->field($generator, 'type', ['template' => '{label}{beginWrapper}{input}{error}{endWrapper}{hint}', 'horizontalCssClasses' => ['wrapper'=>'col-sm-9 col-xs-12 col-12']])
 	->dropDownList($generator->optsType())
 	->label($generator->getAttributeLabel('type'));
 
-echo $form->field($generator, 'keywords', ['template' => '{label}{beginWrapper}{input}{error}{endWrapper}{hint}', 'horizontalCssClasses' => ['wrapper'=>'col-md-9 col-sm-9 col-xs-12 col-12']])
+echo $form->field($generator, 'keywords', ['template' => '{label}{beginWrapper}{input}{error}{endWrapper}{hint}', 'horizontalCssClasses' => ['wrapper'=>'col-sm-9 col-xs-12 col-12']])
 	->label($generator->getAttributeLabel('keywords'));
 
-echo $form->field($generator, 'license', ['template' => '{label}{beginWrapper}{input}{error}{endWrapper}{hint}', 'horizontalCssClasses' => ['wrapper'=>'col-md-9 col-sm-9 col-xs-12 col-12']])
+echo $form->field($generator, 'license', ['template' => '{label}{beginWrapper}{input}{error}{endWrapper}{hint}', 'horizontalCssClasses' => ['wrapper'=>'col-sm-9 col-xs-12 col-12']])
 	->dropDownList($generator->optsLicense(), ['prompt'=>'Choose...'])
 	->label($generator->getAttributeLabel('license'));
 
-echo $form->field($generator, 'title', ['template' => '{label}{beginWrapper}{input}{error}{endWrapper}{hint}', 'horizontalCssClasses' => ['wrapper'=>'col-md-9 col-sm-9 col-xs-12 col-12']])
+echo $form->field($generator, 'title', ['template' => '{label}{beginWrapper}{input}{error}{endWrapper}{hint}', 'horizontalCssClasses' => ['wrapper'=>'col-sm-9 col-xs-12 col-12']])
 	->label($generator->getAttributeLabel('title'));
 
-echo $form->field($generator, 'description', ['template' => '{label}{beginWrapper}{input}{error}{endWrapper}{hint}', 'horizontalCssClasses' => ['wrapper'=>'col-md-9 col-sm-9 col-xs-12 col-12']])
+echo $form->field($generator, 'description', ['template' => '{label}{beginWrapper}{input}{error}{endWrapper}{hint}', 'horizontalCssClasses' => ['wrapper'=>'col-sm-9 col-xs-12 col-12']])
 	->label($generator->getAttributeLabel('description'));
 
-echo $form->field($generator, 'authorName', ['template' => '{label}{beginWrapper}{input}{error}{endWrapper}{hint}', 'horizontalCssClasses' => ['wrapper'=>'col-md-9 col-sm-9 col-xs-12 col-12']])
+echo $form->field($generator, 'authorName', ['template' => '{label}{beginWrapper}{input}{error}{endWrapper}{hint}', 'horizontalCssClasses' => ['wrapper'=>'col-sm-9 col-xs-12 col-12']])
 	->label($generator->getAttributeLabel('authorName'));
 
-echo $form->field($generator, 'authorEmail', ['template' => '{label}{beginWrapper}{input}{error}{endWrapper}{hint}', 'horizontalCssClasses' => ['wrapper'=>'col-md-9 col-sm-9 col-xs-12 col-12']])
+echo $form->field($generator, 'authorEmail', ['template' => '{label}{beginWrapper}{input}{error}{endWrapper}{hint}', 'horizontalCssClasses' => ['wrapper'=>'col-sm-9 col-xs-12 col-12']])
 	->label($generator->getAttributeLabel('authorEmail'));
 
-echo $form->field($generator, 'outputPath', ['template' => '{label}{beginWrapper}{input}{error}{endWrapper}{hint}', 'horizontalCssClasses' => ['wrapper'=>'col-md-9 col-sm-9 col-xs-12 col-12']])
+echo $form->field($generator, 'outputPath', ['template' => '{label}{beginWrapper}{input}{error}{endWrapper}{hint}', 'horizontalCssClasses' => ['wrapper'=>'col-sm-9 col-xs-12 col-12']])
 	->label($generator->getAttributeLabel('outputPath'));
 ?>
--- module/form.php
+++ module/form.php
@@ -3,25 +3,25 @@
 /* @var $form yii\widgets\ActiveForm */
 /* @var $generator yii\gii\generators\module\Generator */
 
-echo $form->field($generator, 'moduleClass', ['template' => '{label}{beginWrapper}{input}{error}{endWrapper}{hint}', 'horizontalCssClasses' => ['wrapper'=>'col-md-9 col-sm-9 col-xs-12 col-12']])
+echo $form->field($generator, 'moduleClass', ['template' => '{label}{beginWrapper}{input}{error}{endWrapper}{hint}', 'horizontalCssClasses' => ['wrapper'=>'col-sm-9 col-xs-12 col-12']])
 	->label($generator->getAttributeLabel('moduleClass'));
 
-echo $form->field($generator, 'moduleID', ['template' => '{label}{beginWrapper}{input}{error}{endWrapper}{hint}', 'horizontalCssClasses' => ['wrapper'=>'col-md-9 col-sm-9 col-xs-12 col-12']])
+echo $form->field($generator, 'moduleID', ['template' => '{label}{beginWrapper}{input}{error}{endWrapper}{hint}', 'horizontalCssClasses' => ['wrapper'=>'col-sm-9 col-xs-12 col-12']])
 	->label($generator->getAttributeLabel('moduleID'));
 
-echo $form->field($generator, 'description', ['template' => '{label}{beginWrapper}{input}{error}{endWrapper}{hint}', 'horizontalCssClasses' => ['wrapper'=>'col-md-9 col-sm-9 col-xs-12 col-12']])
+echo $form->field($generator, 'description', ['template' => '{label}{beginWrapper}{input}{error}{endWrapper}{hint}', 'horizontalCssClasses' => ['wrapper'=>'col-sm-9 col-xs-12 col-12']])
 	->label($generator->getAttributeLabel('description'));
 
-echo $form->field($generator, 'keyword', ['template' => '{label}{beginWrapper}{input}{error}{endWrapper}{hint}', 'horizontalCssClasses' => ['wrapper'=>'col-md-9 col-sm-9 col-xs-12 col-12']])
+echo $form->field($generator, 'keyword', ['template' => '{label}{beginWrapper}{input}{error}{endWrapper}{hint}', 'horizontalCssClasses' => ['wrapper'=>'col-sm-9 col-xs-12 col-12']])
 	->label($generator->getAttributeLabel('keyword'));
 
-echo $form->field($generator, 'moduleCore', ['horizontalCssClasses' => ['wrapper'=>'col-md-9 col-sm-9 col-xs-12 col-12']])
+echo $form->field($generator, 'moduleCore', ['horizontalCssClasses' => ['wrapper'=>'col-sm-9 col-xs-12 col-12']])
 	->checkbox()
 	->label($generator->getAttributeLabel('moduleCore'));
 
-echo $form->field($generator, 'useModified', ['horizontalCssClasses' => ['wrapper'=>'col-md-9 col-sm-9 col-xs-12 col-12']])
+echo $form->field($generator, 'useModified', ['horizontalCssClasses' => ['wrapper'=>'col-sm-9 col-xs-12 col-12']])
 	->checkbox()
 	->label($generator->getAttributeLabel('useModified'));
 
-echo $form->field($generator, 'link', ['template' => '{label}{beginWrapper}{input}{error}{endWrapper}{hint}', 'horizontalCssClasses' => ['wrapper'=>'col-md-9 col-sm-9 col-xs-12 col-12']])
+echo $form->field($generator, 'link', ['template' => '{label}{beginWrapper}{input}{error}{endWrapper}{hint}', 'horizontalCssClasses' => ['wrapper'=>'col-sm-9 col-xs-12 col-12']])
 	->label($generator->getAttributeLabel('link'));
