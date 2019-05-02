0bfd025eed927f16bc152f02c4e276185bf2c3f0
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-05-02 17:15:15 +0700

    change gridview class location

--- crud/gentelella/views/admin_manage.php
+++ crud/gentelella/views/admin_manage.php
@@ -71,7 +71,7 @@ echo "<?php\n";
 
 use yii\helpers\Html;
 use yii\helpers\Url;
-use <?= ($generator->indexWidgetType === 'grid' ? "app\\components\\widgets\\GridView" : "yii\\widgets\\ListView"); ?>;
+use <?= ($generator->indexWidgetType === 'grid' ? "app\\components\\grid\\GridView" : "yii\\widgets\\ListView"); ?>;
 <?= $generator->enablePjax ? 'use yii\widgets\Pjax;'."\n" : ''; ?>
 <?php if(!empty($arrayRelation)):
 echo 'use yii\widgets\DetailView;'."\n";
