a3600a874b3468aa8d35d840c7a6e4aeeff67caa
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-05-03 10:27:56 +0700

    drop layout

--- crud/gentelella/views/admin_manage.php
+++ crud/gentelella/views/admin_manage.php
@@ -342,7 +342,6 @@ array_push($columnData, [
 echo GridView::widget([
 	'dataProvider' => $dataProvider,
 <?= !empty($generator->searchModelClass) ? "\t'filterModel' => \$searchModel,\n" : ''; ?>
-	'layout' => '<div class="row"><div class="col-sm-12">{items}</div></div><div class="row sum-page"><div class="col-sm-5">{summary}</div><div class="col-sm-7">{pager}</div></div>',
 	'columns' => $columnData,
 ]); ?>
 <?php else: ?>
